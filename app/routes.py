62a688e49e56bcefe01bbbfbd21fead6fa802989
Al Taaj <altaaj23@gmail.com>
2025-04-17 10:40:37 +0700

    feat: add caching if prediction already exists

--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,16 @@ from flask import Blueprint, request, jsonify
 from .models import PredictionLog
 from . import db
 from .logic import get_prediction, get_prediction_all
+from datetime import datetime, time
+from sqlalchemy import and_
 
 main = Blueprint("main", __name__)
+
 
 @main.route("/")
 def hello_world():
     return "<p>Hello, Safe Pick!</p>"
+
 
 @main.route("/predict", methods=["POST"])
 def predict():
@@ -19,6 +23,26 @@ def predict():
     if not amount or not tenor:
         return jsonify({"error": "amount and tenor required"}), 400
 
+    # --- Caching logic: check if prediction already exists in db for today after 2 AM UTC (9 AM GMT+7) ---
+    now = datetime.utcnow()
+    today_2am_utc = datetime.combine(now.date(), time(2, 0))  # 2 AM UTC == 9 AM GMT+7
+
+    cached_log = (
+        PredictionLog.query.filter(
+            and_(
+                PredictionLog.amount == amount,
+                PredictionLog.tenor == tenor,
+                PredictionLog.timestamp >= today_2am_utc,
+            )
+        )
+        .order_by(PredictionLog.timestamp.desc())
+        .first()
+    )
+
+    if cached_log:
+        return jsonify(cached_log.to_predict_result_dict())
+
+    # --- no cache: use machine learning model to predict ---
     result = get_prediction(amount, tenor)
     result["profit_gold"] = round(float(result["profit_gold"]), 2)
     result["predicted_gold_price"] = round(float(result["predicted_gold_price"]), 2)
@@ -28,8 +52,12 @@ def predict():
         amount=amount,
         tenor=tenor,
         predicted_price=result["predicted_gold_price"],
+        predicted_buyback=result["predicted_buyback"],
+        gold_gram=result["gold_gram"],
         profit_gold=result["profit_gold"],
         profit_deposit=result["profit_deposit"],
+        gold_return_rate=result["gold_return_rate"],
+        deposit_return_rate=result["deposit_return_rate"],
         recommendation=result["recommend"],
         ip_address=ip_address,
     )
@@ -45,6 +73,7 @@ def get_logs():
     logs = PredictionLog.query.order_by(PredictionLog.timestamp.desc()).limit(100).all()
     return jsonify([log.to_dict() for log in logs])
 
+
 @main.route("/predicts-all", methods=["POST"])
 def predict_all():
     data = request.get_json()
@@ -52,9 +81,7 @@ def predict_all():
 
     if not amount:
         return jsonify({"error": "amount and tenor required"}), 400
-    
+
     result = get_prediction_all(amount)
-    
+
     return jsonify(result)
-
-
